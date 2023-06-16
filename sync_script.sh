019e65b5646d04f7da0984ac798b91c2c1aa864e
kroschu <tukroschu@gmail.com>
2023-06-16 10:25:58 +0300

    Sync: 2023-06-16-10:25:58

--- sync_script.sh
+++ sync_script.sh
@@ -1,3 +1,22 @@
 #!/bin/bash
-# cd storage/shared/LifeWiki
-git add -A && git commit -a -m "android vault backup: `date +'%Y-%m-%d %H-%M-%S'`" && git push
+
+# Збереження поточного каталогу
+current_dir=$(pwd)
+
+# Оновлення сховища
+git pull --rebase
+
+# Додавання всіх змінені файлів до індексу
+git add .
+
+# Збереження змін у коміті
+timestamp=$(date +%Y-%m-%d-%H:%M:%S)
+git commit -m "Sync: $timestamp"
+
+# Відправка змін до сховища
+git push
+
+# Повернення до початкового каталогу
+cd "$current_dir"
+
+echo "Синхронізація завершена."
